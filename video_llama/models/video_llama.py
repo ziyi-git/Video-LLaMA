8d1eb13a422ff05e78b09d0be8fde6bbfbd18e4f
root <lc_lc2000@hotmail.com>
2023-12-12 17:55:03 +0800

    跑通demo_audiovideo.py.

--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -30,7 +30,8 @@ class VideoLLAMA(Blip2Base):
 
     @classmethod
     def init_video_Qformer(cls, num_query_token, vision_width,num_hidden_layers =2):
-        encoder_config = BertConfig.from_pretrained("bert-base-uncased")
+        # encoder_config = BertConfig.from_pretrained("bert-base-uncased")
+        encoder_config = BertConfig.from_pretrained("/workspace/data/bert-base-uncased")
         encoder_config.num_hidden_layers = num_hidden_layers
         encoder_config.encoder_width = vision_width
         # insert cross-attention layer every other block
